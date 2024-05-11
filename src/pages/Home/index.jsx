a2c5aec964c1f9235c7800e1975512b5322e680f
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2024-05-12 02:13:31 +0500

    add files heading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
-import LOGO from "../../assets/logo.svg"
+import LOGO from "../../assets/logo.svg";
 import "./css/style.scss";
 import { MdOutlineTextFields } from "react-icons/md";
 import { FaRegFileAlt } from "react-icons/fa";
-
+import TEXT_GREY from "../../assets/text-grey.svg";
+import TEXT_COLOR from "../../assets/text-color.svg";
+import FILE_GREY from "../../assets/files-grey.svg";
+import FILE_COLOR from "../../assets/files-color.svg";
+import { useState } from "react";
 function HomePage() {
+  const [type, setType] = useState("text");
   return (
     <div className="container">
       <div className="header-bar">
@@ -21,20 +26,25 @@ function HomePage() {
         </div>
       </div>
       <div className="main-card">
-      <div className="card-sidebar">
-      <div className="active">
-      <MdOutlineTextFields size={40}/>
-      </div>
-      <div>
-      <FaRegFileAlt size={35}/>
-      </div>
-   <div>
-   
-   </div>
-      </div>
-      <div className="card-container">
-      
-      </div>
+        <div className="card-sidebar">
+          <div onClick={()=>setType("text")}  className=  {type === "text" && "active"}>
+            <img src={TEXT_COLOR} alt="" />
+          </div>
+          <div  onClick={()=>setType("files")} className=  {type === "files" && "active"}>
+            <img src={FILE_GREY} alt="" />
+          </div>
+        </div>
+        <div className="card-container">
+          {type === "text" ? (
+            <div className="text-section">
+              <h1>Text</h1>
+            </div>
+          ) : (
+            <div className="files-section">
+              <h1>Files</h1>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
